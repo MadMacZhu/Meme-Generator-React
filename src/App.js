fb15086aaf839c1c00ceca4ee7cc43028f3183a0
Yaoguang Zhu <mattzhu1989@gmail.com>
2020-08-18 09:02:15 +0800

    added links at bottom and fixed alignment issues

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import Header from './components/Header';
-import MemeGenerator from './components/MemeGenerator';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import MemeGenerator from "./components/MemeGenerator";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header id="top" />
       <br />
       <MemeGenerator />
+      <a
+        href="https://github.com/MadMacZhu/Meme-Generator-React"
+        target="_blank"
+      >
+        Go to the GitHub page of this site
+      </a>
+      <br />
+      <a href="#top">Go to top</a>
     </div>
   );
 }
